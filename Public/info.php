7d9208e9859db1e56eb4fad0853f4fce021213a6
Kathryn <katmccaf@iu.edu>
2018-10-08 19:13:08 -0400

    Update through table sort video

--- Public/info.php
+++
@@ -1,3 +0,0 @@
-<?php  //We are NEVER going to run PHP in html. SO wel will not close our PHP tag
-
-phpinfo(); //will tell us if php is running
--- app/environment.default.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-const DB_USER = 'root';
-const DB_PW = 'password';
-const DB_NAME = 'mysql:host=localhost;dbname=dashboard_db';
