6df8962331c1aea5f2f9f297d539218ad3459c39
Kathryn <katmccaf@iu.edu>
2018-10-09 01:10:23 -0400

    update through video 11

--- Public/api/work.php
+++ Public/api/work.php
@@ -9,10 +9,10 @@ if ($taskId < 1) {
 }
 
 //1. Go to the database and get all work associated with the $taskId
-$workArr = Work::getAllWorkByTask($taskId);
+$workArr = Work::getWorkByTaskId($taskId);
 
 //2. Convert to Json
-$json = json_encode($workArr);
+$json = json_encode($workArr, JSON_PRETTY_PRINT);
 
 //3. Print
-echo $json; 
+echo $json;
--- app/models/Work.php
+++ app/models/Work.php
@@ -3,24 +3,58 @@
 class Work
 {
   public $id;
-  public $start_date;
-  public $end_date;
+  public $task_id;
+  public $team_id;
+  public $start;
+  public $stop;
+  public $hours;
+  public $completion_estimate;
 
   public function _construct($row) {
-    $this->id = $row['id'];
+    $this->id = intval($row['id']);
 
-    $this->start_date = $row['start_date'];
-    $this->end_date = $row['end_date'];
+    $this->task_id = intval($row['task_id']);
+    $this->team_id = intval($row['team_id']);
 
-    //TODO: Where shuld this be calculated? $this->hours = 0;
+    $this->start = $row['start_date'];
+    $this->hours = floatval($row['hours']);
+
+    //Calculate stop date
+    $hours = floor($this->hours);
+    $mins = intval(($this->hours - $hours) * 60);
+    $interval = 'PT'. ($hours ? $hours. 'H' : '') . ($mins ? $mins. 'M' : '');
+
+    $date = new DateTime($this->start);
+    $date->add(new DateInterval($interval));
+    $this->stop = $date->format('Y-m-d H:i:s');
+
+    $this->completion_estimate = intval($row['completion_estimate']);
   }
 
-  public static function getWorkByTask(int $taskId) {
+  public static function getWorkByTaskId(int $taskId) {
     //1. Connect to the database
-    //2. Run a query
-    //3. Read the results
-    //4. For each row, make a new work object
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    //2. Prepare query
+    $sql = 'SELECT * FROM Work WHERE task_id = ?'; // WHERE date = ? AND ticker = ?';
+
+    $statement = $db->prepare($sql);
+
+    //3. Run the results
+    $success = $statement->execute(
+        [$taskId]
+      );
+
+    //4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      //4a. For each row, make a new work object
+      $workItem = new Work($row);
+
+      array_push($arr, $workItem);
+    }
+
     //5. Return the array of work objects
-    return [];
+    return $arr;
   }
 }
