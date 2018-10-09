12ff92b4fd87c919919a15a75c35d8724509fe3c
Kathryn <katmccaf@iu.edu>
2018-10-08 20:52:43 -0400

    Updatae through 06c - looping and binding with Vue, jsHint config

--- Public/js/dashboard.app.js
+++ Public/js/dashboard.app.js
@@ -8,13 +8,69 @@ var dashboardApp = new Vue({
     "budget" : "4950000",
     "spent" : "3456700",
     "projected_spend" : "4740500",
-    "weekly_effort_target" : 400
+    "weekly_effort_target" : 400,
+
+    tasks: [
+      {
+        "id": 1,
+        "title": "Build UI",
+        "type" : "Story",
+        "size" : "M",
+        "team" : "Noble Carrots",
+        "status": "Started",
+        "start_date": "2018-08-02",
+        "close_date": null,
+        "hours_worked":90,
+        "perc_complete": 95,
+        "current_sprint" : true
+      },
+      {
+        "id": 2,
+        "title": "Update unit tests",
+        "type" : "Story",
+        "size" : "L",
+        "team" : "MS Why Us",
+        "status": "Closed",
+        "start_date": "2018-07-15",
+        "close_date": "2018-08-1",
+        "hours_worked": 40,
+        "perc_complete": 100,
+        "current_sprint" : true
+      },
+      {
+        "id": 3,
+        "title": "Write middleware",
+        "type" : "Epic",
+        "size" : "XL",
+        "team" : "California Dream",
+        "status": "Open",
+        "start_date": null,
+        "close_date": null,
+        "hours_worked": 0,
+        "perc_complete": 0,
+        "current_sprint" : true
+      },
+      {
+        "id": 4,
+        "title": "Completion % not saving",
+        "type" : "Bug",
+        "size" : "XS",
+        "team" : "Luke's Parents",
+        "status": "Open",
+        "start_date": "2018-07-29",
+        "close_date": "",
+        "hours_worked": 17,
+        "perc_complete": 80,
+        "current_sprint" : false
+      }
+    ]
   },
 
   computed: {
     days_left: function () {
       return moment(this.target_date).diff(moment(), 'days')
     },
+
     pretty_target_date: function () {
       return this.pretty_date(this.target_date)
     }
@@ -24,6 +80,7 @@ var dashboardApp = new Vue({
     pretty_date: function (d) {
       return moment(d).format('l')
     },
+
     pretty_currency: function (val) {
       if (val < 1e3) {
         return '$ ' + val
@@ -34,7 +91,16 @@ var dashboardApp = new Vue({
       }
 
       return '$ ' + (val/1e6).toFixed(1) + ' M'
+    },
 
+    complete_class: function(task) {
+      if (task.perc_complete == 100 ) {
+        return 'alert-success'
+      }
+      if (task.current_sprint && task.hours_worked == 0 ) {
+        return 'alert-warning'
+      }
     }
+
   }
 })
