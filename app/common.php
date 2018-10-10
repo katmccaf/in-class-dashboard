3358a9e91e97ccd69cea2155ea72e05698fe4db7
Kathryn <katmccaf@iu.edu>
2018-10-10 12:29:54 -0400

    add api to common

--- app/common.php
+++ app/common.php
@@ -7,5 +7,8 @@ set_include_path (__DIR__);
 require 'environment.php';
 
 /**MODELS **/
-require 'models/Work.php';
+require 'models/Project.php';
+require 'models/Task.php';
 require 'models/Team.php';
+require 'models/Work.php';
+require 'models/WorkHoursReport.php';
